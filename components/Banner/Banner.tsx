ff9bc510f342c569aae6cd6cea96bd19f6bcdef4
Randy Uebel <randy.uebel@zapier.com>
2024-02-14 06:47:01 -0500

    :wrench: fix mobile wrap

--- components/Banner/Banner.tsx
+++ components/Banner/Banner.tsx
@@ -24,23 +24,28 @@ export function Banner({
   const [isStarted, setIsStarted] = useState(!delayStart);
 
   const child = children[childPosition];
-  const sub = isHovered ? child : createConsoleString(child, cursorPos);
+  // If the user is hovering the banner, show the full text
+  // so they can know what they are going to click on
+  const sub = isHovered && href ? child : createConsoleString(child, cursorPos);
 
   useEffect(() => {
-    if (animate && !isHovered) {
+    if (animate && (!isHovered || !href)) {
       let timer: NodeJS.Timeout;
 
       if (!isStarted) {
+        // This timer is used to delay the start of the animation
         timer = setTimeout(() => {
           setIsStarted(true);
         }, randomTime(1000, 2000));
       } else if (cursorPos < child.length) {
+        // This timer is used to animate the cursor
         timer = setTimeout(() => {
           setCursorPos((p) => (p + 1) % (child.length + 1));
         }, randomTime(100, 300));
 
         return () => clearTimeout(timer);
       } else if (children.length > 1) {
+        // This timer is used to swap alternate versions of the text
         timer = setTimeout(() => {
           setChildPosition((p) => (p + 1) % children.length);
           setCursorPos(0);
@@ -62,7 +67,7 @@ export function Banner({
     </Link>
   ) : (
     <div
-      className={styles.banner}
+      className={classes([styles.banner, styles.bannerHighlight])}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
@@ -71,10 +76,16 @@ export function Banner({
   );
 }
 
+/**
+ * Wraps each character in a div to create a spaced effect
+ */
 function createSpaced(text: string) {
   return text.split("").map((t, i) => <div key={i}>{t}</div>);
 }
 
+/**
+ * Creates a string with a cursor at the given position
+ */
 function createConsoleString(text: string, pos: number) {
   return text
     .split("")
@@ -82,6 +93,9 @@ function createConsoleString(text: string, pos: number) {
     .join("");
 }
 
+/**
+ * Generates a random time (number) between min and max
+ */
 function randomTime(min: number, max: number) {
   return Math.random() * (max - min) + min;
 }
